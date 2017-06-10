12a34e52e828325e213186b22a781963f175fb10
Lucient Lau <pursueexcel@gmail.com>
2017-06-10 22:38:00 +0800

    Add POST and Learn CRSF

--- functional_tests.py
+++ functional_tests.py
@@ -33,13 +33,15 @@ class NewVisitorTest(unittest.TestCase):
 
 		table = self.browser.find_element_by_id('id_list_table')
 
-		rows = table.find_element_by_tag_name('tr')
+		rows = table.find_elements_by_tag_name('tr')
 
 		self.assertTrue(
-			any(row.text == '1: Buy peacock feather' for row in rows)
+			any(row.text == '1:Buy peacock feathers' for row in rows),
+			"New to-do item did not appear in table -- its text was:\n%s"%(
+				table.text,
+				)
 			)
 		
-
 		self.fail('Finish the test!')
 
 
