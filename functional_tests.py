aa83d22b1d355670ce19a44f0eb6cb2878b36560
Lucient Lau <pursueexcel@gmail.com>
2017-06-10 09:46:55 +0800

    Modify function_tests.py make test pass

--- functional_tests.py
+++ functional_tests.py
@@ -6,7 +6,7 @@ browser = webdriver.Firefox()
 
 browser.get('http://localhost:8000')
 
-assert 'To-Do' in browser.title, "Browser title was " + browser.title
+assert 'Welcome' in browser.title, "Browser title was " + browser.title
 
 #assert 'Django' in browser.tittle
 
