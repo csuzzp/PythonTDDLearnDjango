d95c3c1897409d07fdca80d96c85e7f906c9ae5c
Lucient Lau <pursueexcel@gmail.com>
2017-06-10 21:39:21 +0800

    Front page HTML now generate from a template

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 #coding = utf-8
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -17,7 +18,7 @@ class NewVisitorTest(unittest.TestCase):
 
 		self.assertIn('To-Do',self.browser.title)
 		
-		header_text = self.browser.find_element_by_tag_name('h1').header_text
+		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('To-Do',header_text)
 
 		inputbox = self.browser.find_element_by_id('id_new_item')
