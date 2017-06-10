0c58047c820e3c2d8a8e3bca3d767c7126f58808
Lucient Lau <pursueexcel@gmail.com>
2017-06-10 09:45:06 +0800

    Modify django function_tests.py add fail message

--- functional_tests.py
+++ functional_tests.py
@@ -6,4 +6,8 @@ browser = webdriver.Firefox()
 
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.tittle
+assert 'To-Do' in browser.title, "Browser title was " + browser.title
+
+#assert 'Django' in browser.tittle
+
+browser.quit()
