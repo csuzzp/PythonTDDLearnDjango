214c5122840c0289e28c121d9fdf9a6f73870a2d
Lucient Lau <pursueexcel@gmail.com>
2017-06-11 15:50:08 +0800

    Make functional_tests an app, use LiveServerTestCase
      **Add
      - make functional_test by using LiveServerTestCase,
        it make the functional test more convenient.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,12 @@
 #coding = utf-8
 
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
+import time
+#import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -14,7 +16,7 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.quit()
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
-		self.browser.get('http://localhost:8000')
+		self.browser.get(self.live_server_url)
 
 		self.assertIn('To-Do',self.browser.title)
 		
@@ -31,6 +33,7 @@ class NewVisitorTest(unittest.TestCase):
 
 		inputbox.send_keys(Keys.ENTER)
 
+		time.sleep(1)
 		table = self.browser.find_element_by_id('id_list_table')
 
 		rows = table.find_elements_by_tag_name('tr')
@@ -45,6 +48,4 @@ class NewVisitorTest(unittest.TestCase):
 		self.fail('Finish the test!')
 
 
-if __name__ == '__main__':
-	unittest.main(warnings='ignore')
 		
