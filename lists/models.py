f2c99e6ce8588d491d5a859e952b9ca496d8eb82
Lucient Lau <pursueexcel@gmail.com>
2017-06-12 19:58:31 +0800

    Modify model let work relate to diff lists

--- lists/models.py
+++ lists/models.py
@@ -1,6 +1,10 @@
 from django.db import models
-
+#from django.db.utils import lists_list
 # Create your models here.
 
+class List(models.Model):
+	pass
+	
 class Item(models.Model):
 	text = models.TextField(default='')
+	list = models.ForeignKey(List,default=None)
